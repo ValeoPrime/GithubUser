d8648abca7b36c7ebf64f7f437f72b5fa854ef57
Valentin <primarh111@mail.ru>
2020-05-17 13:46:28 +0300

    Билд

---
+++ build/precache-manifest.dc2aea816ad801b7bcd5bcf3ff66f22f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "333387175c6ba7f564ada3630f0195f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65c1600f24362de06f13",
+    "url": "/static/css/main.69cd1679.chunk.css"
+  },
+  {
+    "revision": "46fe272539e6ba95c867",
+    "url": "/static/js/2.0206c61a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0206c61a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65c1600f24362de06f13",
+    "url": "/static/js/main.b1323e1d.chunk.js"
+  },
+  {
+    "revision": "a1780faca59c5b13eb63",
+    "url": "/static/js/runtime-main.73fe85f3.js"
+  }
+]);
